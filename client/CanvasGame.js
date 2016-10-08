92b86747434ffa05e842c3f2ea27fce6ea0a991f
Louis Clergue <louisclergue@gmail.com>
2016-10-08 10:14:12 +0200

    add background

--- client/CanvasGame.js
+++ client/CanvasGame.js
@@ -1,9 +1,16 @@
 export class CanvasGame {
 	constructor(canvas) {
 		window.createjs = createjs;
-		this.stage = new createjs.Stage(canvas)
-		createjs.Ticker.addEventListener(this.onTick);
+		window.stage = this.stage = new createjs.Stage(canvas)
+		this.tileSize = 20; // size in pixel
+		this.numTiles = 100;
+		createjs.Ticker.addEventListener('tick', this.onTick.bind(this));
+		this.createBackground()
 		this.onResize();
+	}
+	createBackground() {
+		this.bg = new createjs.Shape();
+		this.stage.addChild(this.bg);
 	}
 	onTick() {
 		this.stage.update();
@@ -11,5 +18,10 @@ export class CanvasGame {
 	onResize(event) {
 		this.stage.canvas.width = window.innerWidth;
 		this.stage.canvas.height = window.innerHeight;
+		this.center = {
+			x: this.stage
+		}
+		this.bg.graphics.clear();
+		this.bg.graphics.beginFill('#222').drawRect(0,0,this.stage.canvas.width, this.stage.canvas.height);
 	}
 }
